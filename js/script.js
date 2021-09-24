3c68435b297d3f963b5d872efd21ff377bca875b
situhin2007 <situhin2007@gmail.com>
2021-09-25 01:12:50 +0600

    reviewSectionDone

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active')
 }
 
- var swiper = new Swiper(".home-slider", {
+var swiper = new Swiper(".home-slider", {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
@@ -30,5 +30,29 @@ document.querySelector('#close').onclick = () =>{
           el: ".swiper-pagination",
           clickable: true,
         },
-       loop:true,
-      });
+      loop:true,
+    });
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
